34ae90242506120d79e704b0cd209334a1e22bd6
harshsankhla18 <harsh18sankhlamy@gmail.com>
2026-09-12 14:14:35 +0530

    feat : Connected to neon using Drizzle ORM

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import express from 'express';
-
 const app = express();
+
 const PORT  = 8000;
 
-// Use JSON middleware
 app.use(express.json());
 
-// Root GET route
 app.get('/', (req, res) => {
   res.json({ message: 'Hello from Sportboard server!' });
 });
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
